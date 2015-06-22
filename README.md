389e51ea4be20c5520a9d5595bda457cae3fdb3d
Gavin Lloyd <gavinhungry@gmail.com>
2015-06-22 12:25:57 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+rudiment
+========
+A simple RESTful resource manager.
+
+License
+-------
+Released under the terms of the
+[MIT license](http://tldrlegal.com/license/mit-license). See **LICENSE**.
---
+++ rudiment.js
@@ -0,0 +1,15 @@
+/**
+ * rudiment - A simple RESTful resource manager
+ * https://github.com/gavinhungry/rudiment
+ */
+
+(function() {
+  'use strict';
+
+  var Rudiment = function(opts) {
+
+  };
+
+  module.exports = Rudiment;
+
+})();
