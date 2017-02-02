2ea40cf743ecc9add37e5e7416dc0193a3779c8c
Gavin Lloyd <gavinhungry@gmail.com>
2017-02-02 00:52:58 -0800

    getNextIndex rejects with error with no index

--- rudiment.js
+++ rudiment.js
@@ -162,7 +162,7 @@
      */
     getNextIndex: function() {
       if (!this._index) {
-        return Promise.resolve(null);
+        return Promise.reject(new Error('No auto-indexing key specified'));
       }
 
       return this._dbApi.getNextIndex();
